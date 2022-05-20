8c26e65544b15ebfa63ce7eadfa1130cf423d42c
Hp <adilthebestw27@gmail.com>
2022-05-20 23:59:59 +0300

    .

--- app/src/main/java/com/example/hw1a2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/hw1a2/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.hw1a2.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.hw1a2.R
 import com.example.hw1a2.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,17 +25,24 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fap.setOnClickListener {
+            findNavController().navigate(R.id.secondFragment)
         }
-        return root
+        parentFragmentManager.setFragmentResultListener(
+            "rk_news",
+            viewLifecycleOwner
+        ) { key, bundle ->
+            val text = bundle.getString("text")
+            Log.e("Home","text$text")
+        }
     }
 
     override fun onDestroyView() {
